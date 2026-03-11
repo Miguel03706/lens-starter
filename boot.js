1097b1188a3f877dbc260895d23633b304b12bd8
Miguel <75583778+Miguel03706@users.noreply.github.com>
2026-03-11 20:03:36 -0300

    teste

--- boot.js
+++ boot.js
@@ -8,14 +8,14 @@ var qs = function () {
   var query_string = {};
   var query = window.location.search.substring(1);
   var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
+  for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split("=");
-      // If first entry with this name
+    // If first entry with this name
     if (typeof query_string[pair[0]] === "undefined") {
       query_string[pair[0]] = pair[1];
       // If second entry with this name
     } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]], pair[1] ];
+      var arr = [query_string[pair[0]], pair[1]];
       query_string[pair[0]] = arr;
       // If third or later entry with this name
     } else {
@@ -23,14 +23,14 @@ var qs = function () {
     }
   }
   return query_string;
-} ();
+}();
 
 // This document gets loaded by default
 // --------
 
-var documentURL = "data/example.xml";
+var documentURL = "data/artigo3.xml";
 
-$(function() {
+$(function () {
 
   // Create a new Lens app instance
   // --------
